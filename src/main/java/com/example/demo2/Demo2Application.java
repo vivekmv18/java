769aafc8a9c3588edbfbffe55240d7694ab109ba
vivekmv18 <vivek.mv@outlook.com>
2025-04-27 14:28:03 +0530

    Initial commit

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,24 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RequestMapping("/spring-boot")
+@RestController
+public class Demo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String First()
+{
+return "Vivek";
+}
+
+}
